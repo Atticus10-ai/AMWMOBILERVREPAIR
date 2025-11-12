53618b790b6a74ab18cc23c8f407d885c62276fa
dave <dhofer@intoil.com>
2025-11-11 20:06:19 -0500

    Misc

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './',
+  base: '/',
+  server: {
+    port: 3000,
+    open: true,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
+});
+
+
